6238b92a8c8c063d84079aa2b5455b74b4b42ae7
Pablo Henrique Bertaco <pablo_fonseca91@icloud.com>
2017-02-14 14:42:56 -0200

    working on ChooseMissionScene design

---
+++ Xcode/Shared/Scenes/ChooseMissionScene.swift
@@ -0,0 +1,13 @@
+//
+//  ChooseMissionScene.swift
+//  GameVI
+//
+//  Created by Pablo Henrique Bertaco on 2/9/17.
+//  Copyright © 2017 PabloHenri91. All rights reserved.
+//
+
+import SpriteKit
+
+class ChooseMissionScene: GameScene {
+
+}
