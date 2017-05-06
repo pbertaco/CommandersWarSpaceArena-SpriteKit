a89af5cdc01f1343eeb3bda6c866f0573afe430a
Pablo Henrique Bertaco <pablo_fonseca91@icloud.com>
2017-05-05 21:00:58 -0300

    Add gift ship on game update revert new icon

--- Xcode/Shared/CoreData/PlayerData.swift
+++ Xcode/Shared/CoreData/PlayerData.swift
@@ -46,5 +46,29 @@ extension MemoryCard {
         if self.playerData.modelVersion < 1 {
             self.playerData.modelVersion = 1
         }
+        
+        if self.playerData.modelVersion < 2 {
+            
+            self.playerData.modelVersion = 2
+            
+            self.playerData.points = self.playerData.points + 1000000
+            self.playerData.premiumPoints = self.playerData.premiumPoints + 1000
+            
+            let spaceshipData: SpaceshipData = self.newSpaceshipData(rarity: .legendary)
+            
+            spaceshipData.baseDamage = 36
+            spaceshipData.baseLife = 1815
+            spaceshipData.baseRange = 64
+            spaceshipData.baseSpeed = 18
+            
+            spaceshipData.colorRed = 1.0
+            spaceshipData.colorGreen = 0.75
+            spaceshipData.colorBlue = 0
+            
+            spaceshipData.level = 10
+            spaceshipData.skin = 13
+            
+            playerData.addToSpaceships(spaceshipData)
+        }
     }
 }
